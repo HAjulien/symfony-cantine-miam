84ac3cdf9fceb4c8010297be58f1d471b18e8186
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-07-09 21:10:26 +0200

    securite route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,7 +16,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(ProduitRepository $ProduitRepository, UserRepository $userRepository, CommandeRepository $commandeRepository, SelectionRepository $selectionRepository, CritiqueRepository $critiqueRepository): Response
     {
-        
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $today = date("d/m/y");
         $today2 = date("y-m-d");
         $mois = date("y-m");
